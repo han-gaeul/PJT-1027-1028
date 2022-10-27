17fd1985f65779ce0430479c6203578cf6105c18
han-gaeul <gmlakdcjs314@gmail.com>
2022-10-27 21:36:44 +0900

    accounts - create login

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ app_name = 'reviews'
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import redirect, render
 from .forms import CustomCreationForm
 from .models import Profile
 from django.contrib.auth import login as auth_login
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
 
@@ -22,3 +23,19 @@ def signup(request):
         'form' : form
     }
     return render(request, 'accounts/signup.html', context)
+
+# 로그인
+def login(request):
+    if request.user.is_authenticated:
+        return redirect('reviews:index')
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect(request.GET.get('next') or 'reviews:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form' : form
+    }
+    return render(request, 'accounts/login.html', context)
