5ed1eea801b62bfee4140ed10933cb03782de5ae
han-gaeul <gmlakdcjs314@gmail.com>
2022-10-27 22:08:50 +0900

    reviews - index

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Review, Comment
+from reviews.models import Review
+from django.db.models import Count
 
 # Create your views here.
+
+# 글 목록
+def index(request):
+    reviews = Review.objects.annotate(Count('commnet')).order_by('-pk')
+    context = {
+        'reviews' : reviews
+    }
+    return render(request, 'reviews/index.html', context)
